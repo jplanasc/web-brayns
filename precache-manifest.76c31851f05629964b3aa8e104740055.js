f44a814df77503145b680f7d7642d861b4c6c42b
Fabien Petitjean <petitjea@cbdhcp-2-046.epfl.ch>
2019-07-11 18:54:02 +0200

    Updates

---
+++ precache-manifest.76c31851f05629964b3aa8e104740055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "./static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3541aa35b8a8e3cefd5b",
+    "url": "./static/js/main.058383c7.chunk.js"
+  },
+  {
+    "revision": "3d413f17b4568a59e3d3",
+    "url": "./static/js/2.c5f731e3.chunk.js"
+  },
+  {
+    "revision": "3541aa35b8a8e3cefd5b",
+    "url": "./static/css/main.81e0614d.chunk.css"
+  },
+  {
+    "revision": "5fed099b65ef2a6eb0ad3138339ae7d2",
+    "url": "./index.html"
+  }
+];
