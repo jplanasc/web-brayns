d60e6316407951ec6ec4c4f84e0334646db9674d
Tolokoban <fabien.petitjean@epfl.ch>
2019-10-23 17:01:45 +0200

    Added input dialogs for files and directories.

---
+++ src/web-brayns/dialog/file.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import Dialog from '../../tfw/factory/dialog'
+import InputPath from '../view/input-path'
+
+interface IParams {
+    title?: string,
+    storageKey: string
+}
+
+export default {
+    async show(arg: Partial<IParams>): Promise<string | null> {
+        return new Promise(resolve => {
+            const opt: IParams = {
+                storageKey: "default",
+                ...arg
+            }
+            const content = <InputPath
+                storageKey={opt.storageKey}
+                foldersOnly={false}
+                onLoadClick={path => {
+                    resolve(path)
+                    dialog.hide()
+                }}/>
+
+            const dialog = Dialog.show({
+                title: opt.title,
+                content,
+                closeOnEscape: true,
+                icon: "file",
+                onClose: () => resolve(null)
+            })
+        })
+    }
+}
